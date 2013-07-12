05b39d7bca3750d1b724da03fe81f5b069043ece
Govinda Dasu <govinda@govindadasu.com>
2013-07-12 12:22:59 -0700

    set up fb login button and users table

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var express = require('express')
 var stylus = require('stylus')
 var nib = require('nib')
 var check = require('validator').check,
-    sanitize = require('validator').sanitize
+	sanitize = require('validator').sanitize
+var mongoose = require('mongoose');
+	mongoose.connect('mongodb://localhost/startup_cloud');
 
 // init db and app
 var app = express()
 
 // init jade compiler
 function compile(str, path) {
- return stylus(str)
- .set('filename', path)
- .use(nib())
+	return stylus(str)
+	.set('filename', path)
+	.use(nib())
 }
 
 // init views
@@ -20,22 +22,25 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
 app.use(stylus.middleware({ 
- src: __dirname + 'public',
- compile: compile
+	src: __dirname + 'public',
+	compile: compile
 }))
 
 app.configure('development', function(){
- app.use(express.errorHandler())
- app.locals.pretty = true
+	app.use(express.errorHandler())
+	app.locals.pretty = true
 })
 
 // init static public directory
 app.use(express.static(__dirname + '/public'))
 
 // render pages
- app.get('/', function(req, res){
- res.render('index', {title: 'Startup Cloud'})
+app.get('/', function(req, res){
+	res.render('index', {title: 'Startup Cloud'})
 })
+
+// database interaction
+var User = mongoose.model('User', { fname: String, mname: String, lname: String, fbid: String })
 
 // listen on port
 app.listen(7777)
